e85cdfa36dde12c058829997ecf632d2575a64d0
Developer-Personal <developerpersonal123@gmail.com>
2024-11-10 15:46:12 +0530

    api's structure

---
+++ client/src/api/ApiConstants.js
@@ -0,0 +1,21 @@
+
+const ApiConstants = {
+
+  //POST API
+  REGISTER_USER: () => `/api/auth/register`,
+  LOGIN_USER: () => `/api/auth/login`,
+
+  
+  //GET API
+  CHECK_AUTH : () => `/api/auth/checkAuth`,
+  GET_ALL_USERS : () => `/api/auth/getAllUsers`,
+  
+
+
+  //PUT API
+
+  //DELETE API
+
+};
+
+export default ApiConstants;
