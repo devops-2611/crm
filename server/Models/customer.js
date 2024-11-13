f1a49088a95bc0e02e6de6b4eaa52cfd0d486483
Developer-Personal <developerpersonal123@gmail.com>
2024-11-13 18:33:23 +0530

    customer added for selection

--- server/Models/customer.js
+++ server/Models/customer.js
@@ -1,42 +1,61 @@
 const mongoose = require('mongoose');
 
-const CustomerSchema = new mongoose.Schema({
-    
-  customerId:{
-    type: Number,
-    required: true,
-    unique: true, 
-  },
-  customerName: {
-    type: String,
-    required: true,
-  },
-  customerEmail: {
-    type: String,
-  },
-  customerMobile: {
-    type: String,
-  },
-  customerAddress: {
-    type: String,
-  },
-  configDetails:{
-    serviceFee:{
+const CustomerSchema = new mongoose.Schema(
+  {
+    customerId: {
       type: Number,
+      required: true,
+      unique: true,
+      index: true,
     },
-    deliveryCharge:{
-      type: Number,
+    customerName: {
+      type: String,
+      required: true,
     },
-    driverTip:{
-      type: Number,
+    customerEmail: {
+      type: String,
+      match: /.+\@.+\..+/,
     },
-    surCharge:{
-      type: Number,
+    customerMobile: {
+      type: String,
     },
-    orderDiscount:{
-      type: Number,
-    }
-  }
-});
+    customerAddress: {
+      type: String,
+    },
+    configDetails: {
+      serviceFee: {
+        isApplicable: {
+          type: Boolean,
+          default: true,
+        },
+        value: {
+          type: Number,
+          default: 0,
+        },
+      },
+      deliveryCharge: {
+        isApplicable: {
+          type: Boolean,
+          default: true,
+        },
+        value: {
+          type: Number,
+          default: 0,
+        },
+      },
+      driverTip: {
+        isApplicable: {
+          type: Boolean,
+          default: true,
+        },
+        value: {
+          type: Number,
+          default: 0,
+        },
+      },
+    },
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model('Customer', CustomerSchema);
