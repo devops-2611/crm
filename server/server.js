80d5c460fc764e317725f3417c2bf7ebc30b314f
Developer-Personal <developerpersonal123@gmail.com>
2024-11-26 21:23:29 +0530

    multer configuration

--- server/server.js
+++ server/server.js
@@ -16,8 +16,15 @@ connectDB();
 
 app.use(cors())
 
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ limit: '10mb', extended: true }));
+// app.use(express.json({ limit: '10mb' }));
+// app.use(express.urlencoded({ limit: '10mb', extended: true }));
+app.use((req, res, next) => {
+    if (req.path.startsWith('/api/invoice/uploadAndGetInvoiceData')) {
+        return next();
+    }
+    express.json({ limit: '10mb' })(req, res, next);
+}, express.urlencoded({ limit: '10mb', extended: true }));
+
 
 // Serve static files from the 'Uploads' directory
 app.use('/uploads', express.static(path.join(__dirname, 'Uploads')));
