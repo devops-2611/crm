7863834598b7981b4252093f8c7dc6712b613394
Ubuntu <welcomeuser@welcomevm.aya4hqft5fxe1fua1ufkmbhfog.dx.internal.cloudapp.net>
2025-02-21 05:43:17 +0000

    test website

--- Models/idGenerator.js
+++
@@ -1,9 +0,0 @@
-// Models/IdGenerator.js
-const mongoose = require('mongoose');
-
-const idGeneratorSchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true }, // e.g., "customerId", "merchantId", "itemId"
-  seq: { type: Number, default: 0 },                   // Counter for the ID
-});
-
-module.exports = mongoose.model('IdGenerator', idGeneratorSchema);
--- dist/assets/Homepage-BK5_XGgH.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-DwXd4TTA.js";import{C as o}from"./Container-BWWWe1xS.js";const a=()=>e.jsx(o,{children:"Home page"});export{a as default};
